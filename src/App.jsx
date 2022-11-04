30ffd672653b99e7c7362652094e3b8dd789ddce
Kemal <i.kemal.karabulut@gmail.com>
2022-11-04 19:08:01 +0100

    add user missing

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import woman from "./assets/woman.svg";
 
 function App() {
   const [user, setUser] = useState({});
-  const [show, setShow] = useState("");
+  const [show, setShow] = useState("email");
   const [count, setCount] = useState(0);
 
   useEffect(() => {
@@ -18,51 +18,66 @@ function App() {
       const res = await fetch("https://randomuser.me/api/");
       const data = await res.json();
       let singleUser = data.results[0];
-      // console.log(singleUser);
-      setUser(singleUser);
+
+      let q = [];
+      q.push({
+        name: singleUser.name.first,
+        email: singleUser.email,
+        phone: singleUser.phone,
+        picture: singleUser.picture.large,
+        gender: singleUser.gender,
+        address: singleUser.location.city,
+        age: singleUser.dob.age,
+      });
+
+      setUser(q[0]);
     }
     getData();
   }, [count]);
 
   function handleHover(event) {
-    console.log(event);
-    setShow(event.target.ariaLabel);
-    console.log(typeof show);
-    console.log(show);
+    if (event.target.id) {
+      setShow(event.target.id);
+    }
+    return;
   }
+  useEffect(() => {
+    if (show) {
+      setShow((prev) => prev);
+    }
+  }, [show]);
+  console.log("show value is " + show);
+  //
 
-  console.log(user);
-  const { cell, dob, email, name, phone, picture } = user;
   return (
     <article className="wrapper">
       <div className="container">
         <div className="part1">
-          <img src={picture?.large} alt="" />
+          <img src={user.picture} alt="" />
         </div>
         <div className="part2">
-          <h4 className="property">My something something</h4>
-          <h2 className="data">sonuc {user?.show}</h2>
+          <div className="response">
+            <h4 className="property">
+              {show ? `My ${show} is` : "Hover over to see details"}
+            </h4>
+            <h2 className="data">{user[show]}</h2>
+          </div>
           <div className="hovers">
-            <div
-              className="icon"
-              onMouseEnter={handleHover}
-              id="email"
-              aria-label="email"
-            >
+            <div className="icon" onMouseEnter={handleHover} id="email">
               <img src={mail} alt="" />
             </div>
-            <div className="icon" id="phone">
+            <div className="icon" id="phone" onMouseEnter={handleHover}>
               <img src={phoneicon} alt="" data-label="phone" />
             </div>
 
-            <div className="icon" id="man">
+            <div className="icon" id="name" onMouseEnter={handleHover}>
               <img
                 src={user.gender === "male" ? man : woman}
                 alt=""
                 data-label="man"
               />
             </div>
-            <div className="icon" id="map">
+            <div className="icon" id="address" onMouseEnter={handleHover}>
               <img src={map} alt="" data-label="map" />
             </div>
           </div>
